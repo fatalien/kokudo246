d7806d716ffc86af846429c984ed974bbbc2d565
Zakaria Aziz <zaxstyle@hotmail.com>
2014-06-23 22:39:09 +0900

    for heroku

---
+++ app/models/listing.rb
@@ -0,0 +1,5 @@
+class Listing < ActiveRecord::Base
+	 has_attached_file :image, :styles => { :medium => "400x", :thumb => "200x" }, :default_url => "/images/:style/missing.png"
+	 validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+end
