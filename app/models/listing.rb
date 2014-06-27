39ccbb2727f7734cebe4b717e4e7cabff522ea5c
Zakaria Aziz <zaxstyle@hotmail.com>
2014-06-27 22:23:26 +0900

    addd validation to listings model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -11,6 +11,6 @@ class Listing < ActiveRecord::Base
   validates :price, numericality: { greater_than: 0 }
   validates_attachment_presence :image
 
-  validates_attachment_content_type :file_name, :content_type => %w(image/jpeg image/jpg image/png)
+  validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
 
 end
